94a85621001101359c44cce0a943c2e839216fcf
Markus Jakonen <markus.jakonen@gmail.com>
2026-06-02 12:11:19 +0300

    Add a script to delete all data from all tables from the data warehouse.

---
+++ sql/warehouse/Delete-all-data-from-tables.sql
@@ -0,0 +1,59 @@
+-- ============================================================
+-- Delete all data from all tables in RentalDW
+-- ============================================================
+
+USE RentalDW;
+GO
+
+-- Drop all foreign keys in MiniProject schema
+DECLARE @sql NVARCHAR(MAX) = N'';
+
+SELECT @sql += N'
+ALTER TABLE ' + QUOTENAME(s.name) + '.' + QUOTENAME(t.name) +
+N' DROP CONSTRAINT ' + QUOTENAME(fk.name) + N';'
+FROM sys.foreign_keys fk
+JOIN sys.tables t 
+    ON fk.parent_object_id = t.object_id
+JOIN sys.schemas s 
+    ON t.schema_id = s.schema_id
+WHERE s.name = 'MiniProject';
+
+EXEC sp_executesql @sql;
+GO
+
+-- Drop all remaining constraints: PK, UQ, CK, DEFAULT
+DECLARE @sql NVARCHAR(MAX) = N'';
+
+SELECT @sql += N'
+ALTER TABLE ' + QUOTENAME(s.name) + '.' + QUOTENAME(t.name) +
+N' DROP CONSTRAINT ' + QUOTENAME(c.name) + N';'
+FROM sys.objects c
+JOIN sys.tables t 
+    ON c.parent_object_id = t.object_id
+JOIN sys.schemas s 
+    ON t.schema_id = s.schema_id
+WHERE s.name = 'MiniProject'
+  AND c.type IN ('PK', 'UQ', 'C', 'D');
+
+EXEC sp_executesql @sql;
+GO
+
+-- Optional: delete all data first
+DELETE FROM MiniProject.FactSales;
+DELETE FROM MiniProject.DimItem;
+DELETE FROM MiniProject.DimGeography;
+DELETE FROM MiniProject.DimDate;
+DELETE FROM MiniProject.DimCustomer;
+GO
+
+-- Drop tables
+DROP TABLE IF EXISTS MiniProject.FactSales;
+DROP TABLE IF EXISTS MiniProject.DimItem;
+DROP TABLE IF EXISTS MiniProject.DimGeography;
+DROP TABLE IF EXISTS MiniProject.DimDate;
+DROP TABLE IF EXISTS MiniProject.DimCustomer;
+GO
+
+-- Optional: drop schema if empty
+DROP SCHEMA IF EXISTS MiniProject;
+GO
